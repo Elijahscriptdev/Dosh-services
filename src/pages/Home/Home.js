6762fdd0ca627991d577f222bb4ac68de96d18e8
Emmanuel Nwachi <egreatness1@gmail.com>
2021-04-28 00:45:20 +0100

    Utils: Add assets and imgs

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ReactTitle } from "react-meta-tags";
+
+const Home = () => {
+    return (
+      <>
+        <ReactTitle title="Dosh Services - IT Solutions Service Providers" />
+        <div>
+          <h2>Hello Dosh !!!</h2>
+        </div>
+      </>
+    );
+}
+
+export default Home
