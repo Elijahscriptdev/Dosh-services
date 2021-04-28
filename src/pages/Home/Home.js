c0f50a0b7c6d3327b11642385191b9c467dd7087
Emmanuel Nwachi <egreatness1@gmail.com>
2021-04-28 01:47:09 +0100

    fixes: dependency errors

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { ReactTitle } from "react-meta-tags";
 
+import "../../assets/css/main.css"
+import "../../assets/css/responsive.css";
+import "../../assets/css/slick.css";
+import "../../assets/css/magnific-popup.css";
+import "../../assets/css/meanmenu.css";
+import "../../assets/css/bootstrap.min.css";
+import "../../assets/css/owl.carousel.min.css";
+import "../../assets/css/animate.min.css";
+import "../../assets/css/themify-icons.css";
+
 const Home = () => {
     return (
       <>
